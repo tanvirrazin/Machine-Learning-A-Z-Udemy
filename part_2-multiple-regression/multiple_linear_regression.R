c47835b020e6694db4045169f37898dd82aa5f16
Razin Tanvir <tanvirrazin@gmail.com>
2017-02-12 22:39:13 +0600

    Implemented Backward Elimination for Multiple Linear regression

---
+++ part_2-multiple-regression/multiple_linear_regression.R
@@ -0,0 +1,39 @@
+# Importing the dataset
+# ---------------------
+dataset = read.csv('50_Startups.csv')
+# dataset = dataset[, 2:3]
+
+# Encoding categorical data
+# -------------------------
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# --------------------------------------------------------
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+#Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+summary(regressor)
+y_pred = predict(regressor, newdata=test_set)
+
+# Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend, data = dataset)
+summary(regressor)
