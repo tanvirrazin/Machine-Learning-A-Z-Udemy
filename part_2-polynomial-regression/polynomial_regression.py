baf8d64c2c041e974226bccfe737b91f1c9a020d
Razin Tanvir <tanvirrazin@gmail.com>
2017-02-13 22:45:13 +0600

    Fitted polynomial regressor data to linear regressor

---
+++ part_2-polynomial-regression/polynomial_regression.py
@@ -0,0 +1,23 @@
+# Importing libraries
+# -------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+# ---------------------
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
