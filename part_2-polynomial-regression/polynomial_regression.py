f4c4465eca24bab2ac39b6e5591a11e47dadf141
Razin Tanvir <tanvirrazin@gmail.com>
2017-02-13 23:35:51 +0600

    Predicted using the trained linear and polynomial model

--- part_2-polynomial-regression/polynomial_regression.py
+++ part_2-polynomial-regression/polynomial_regression.py
@@ -21,3 +21,55 @@ poly_reg = PolynomialFeatures(degree=2)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
+# Fitting Polynomial Regression to the dataset with degree=3
+poly_reg = PolynomialFeatures(degree=3)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Polynomial Regression results with degree=3
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
+# Fitting Polynomial Regression to the dataset with degree=4
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Polynomial Regression results with degree=4
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
