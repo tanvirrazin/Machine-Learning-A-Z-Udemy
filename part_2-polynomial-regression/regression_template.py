5e8953f4901da783a2f08ffcd4539d5adc8beaf0
Razin Tanvir <tanvirrazin@gmail.com>
2017-02-18 08:45:03 +0600

    Added regression template for python

---
+++ part_2-polynomial-regression/regression_template.py
@@ -0,0 +1,35 @@
+# Importing libraries
+# -------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+# ---------------------
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting Regression to the dataset
+# create your regressor here
+
+# Predicting a new result with Polynomial Regression
+y_pred = regressor.predict(6.5)
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
