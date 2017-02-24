ff188ccb84723f4f549d87698ddb3b4b5575062f
Razin Tanvir <tanvirrazin@gmail.com>
2017-02-24 12:55:52 +0600

    Added decision-tree-regression in R

---
+++ part_2-decision-tree-regression/decision_tree_regression.R
@@ -0,0 +1,24 @@
+# Importing the dataset
+# ---------------------
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting Decision Tree Regression to the dataset
+library(rpart)
+regressor = rpart(formula = Salary ~ ., data = dataset, control = rpart.control(minsplit = 1))
+
+# Predicting a new result with Decision Tree Regression
+y_pred = predict(regressor, data.frame(Level=6.5))
+
+library(ggplot2)
+
+# Build dataset to smoothly visualize pedicted graph in higher resolution
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+
+# Visualizing the Decision Tree Regression results
+ggplot() + 
+    geom_point(aes(x=dataset$Level, y=dataset$Salary), color='red') +
+    geom_line(aes(x=x_grid, y=predict(regressor, newdata=data.frame(Level=x_grid))), color='blue') +
+    ggtitle('Truth and Bluff (Decision Tree)') +
+    xlab('Level') +
+    ylab('Salary')
