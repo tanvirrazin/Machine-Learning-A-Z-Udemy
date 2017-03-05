f7a5ae1e31e55c3d03d5896524b8d71d56416a81
Razin Tanvir <tanvirrazin@gmail.com>
2017-03-05 20:15:38 +0600

    Implemented random forest regression in python

---
+++ part_2-random-forest-regression/random_forest_regression.py
@@ -0,0 +1,29 @@
+# Importing libraries
+# -------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+# ---------------------
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=300, random_state=0)
+regressor.fit(X, y)
+
+# Predicting a new result with Polynomial Regression
+y_pred = regressor.predict(6.5)
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
