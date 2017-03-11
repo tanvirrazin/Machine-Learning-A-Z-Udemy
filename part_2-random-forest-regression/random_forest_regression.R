5cf0d0edf39be3bc3b9e8e86b41e6d484df74410
Razin Tanvir <tanvirrazin@gmail.com>
2017-03-11 21:53:30 +0600

    Implemented Random Forest regression in R

---
+++ part_2-random-forest-regression/random_forest_regression.R
@@ -0,0 +1,28 @@
+# Importing the dataset
+# ---------------------
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting Regression to the dataset
+# install.packages('randomForest')
+library(randomForest)
+set.seed(1234)
+regressor = randomForest(x=dataset[1],
+						 y=dataset$Salary,
+						 ntree=500)
+
+# Predicting a new result with Polynomial Regression
+y_pred = predict(regressor, data.frame(Level=6.5))
+
+library(ggplot2)
+
+# Build dataset to smoothly visualize pedicted graph in higher resolution
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+
+# Visualizing the Polynomial Regression results
+ggplot() + 
+    geom_point(aes(x=dataset$Level, y=dataset$Salary), color='red') +
+    geom_line(aes(x=x_grid, y=predict(regressor, newdata=data.frame(Level=x_grid))), color='blue') +
+    ggtitle('Truth and Bluff (Random Forest Regression Model)') +
+    xlab('Level') +
+    ylab('Salary')
