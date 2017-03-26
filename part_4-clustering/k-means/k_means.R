7d97573e630de18335a11d4c90c0cddf373d0331
Razin Tanvir <tanvirrazin@gmail.com>
2017-03-26 13:11:53 +0600

    Implemented k-means with R

---
+++ part_4-clustering/k-means/k_means.R
@@ -0,0 +1,29 @@
+# K-means clustering
+
+# Importing the mall dataset
+dataset <- read.csv('../../data_files/Mall_customers.csv')
+X <- dataset[4:5]
+
+# Using Elbow Method to find the optimal number of clusters
+set.seed(6)
+wcss <- vector()
+for (i in 1:10) wcss[i] <- sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type="b", main=paste('Clusters of clients'), xlab="Number of clusters", ylab="WCSS")
+
+# Applying k-means to the mall dataset
+set.seed(29)
+kmeans <- kmeans(X, 5, iter.max=300, nstart=10)
+
+# Visualizing the clusters
+library(cluster)
+clusplot(X,
+        kmeans$cluster,
+        lines=0,
+        shade=TRUE,
+        color=TRUE,
+        labels=2,
+        plotchar=FALSE,
+        span=TRUE,
+        main=paste('Clusters of clients'),
+        xlab="Annual Income",
+        ylab="Spending Score")
