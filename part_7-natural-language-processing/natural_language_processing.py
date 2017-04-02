07f66754ae740ea9c46e13b5cca25e271f769975
Razin Tanvir <tanvirrazin@gmail.com>
2017-04-02 12:59:29 +0600

    Classified texts with Gaussian-Naive-Bayes classifier and saw confusion matrix

--- part_7-natural-language-processing/natural_language_processing.py
+++ part_7-natural-language-processing/natural_language_processing.py
@@ -29,3 +29,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Fitting classifier to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
