329ccfc7671e1d16d867a386cdec96faa009454f
Razin Tanvir <tanvirrazin@gmail.com>
2017-04-14 17:51:53 +0600

    Implemented LDA in R

--- part_9-dimentionality-reduction/linear_discriminant_analysis/lda.R
+++ part_9-dimentionality-reduction/linear_discriminant_analysis/lda.R
@@ -18,21 +18,20 @@ test_set = subset(dataset, split==FALSE)
 training_set[-14] = scale(training_set[-14])
 test_set[-14] = scale(test_set[-14])
 
-# Applying PCA
+# Applying LDA
 # ------------
-library(caret)
-library(e1071)
-pca = preProcess(x=training_set[-14], method='pca', pcaComp=2)
-training_set = predict(pca, training_set)
-training_set = training_set[c(2, 3, 1)]
-test_set = predict(pca, test_set)
-test_set = test_set[c(2, 3, 1)]
+library(MASS)
+lda = lda(formula=Customer_Segment ~ ., data=training_set)
+training_set = as.data.frame(predict(lda, training_set))
+training_set = training_set[c(5, 6, 1)]
+test_set = as.data.frame(predict(lda, test_set))
+test_set = test_set[c(5, 6, 1)]
 
 # Fitting SVM Classifier to training set
 #---------------------------------------
 # Create your own classifier here
 library(e1071)
-classifier = svm(formula=Customer_Segment ~ .,
+classifier = svm(formula=class ~ .,
                  data=training_set,
                  type='C-classification',
                  kernel='linear')
@@ -48,15 +47,14 @@ cm = table(test_set[, 3], y_pred)
 #-------------------------------------
 library(ElemStatLearn)
 set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1] + 1, by=0.01))
-X2 = seq(min(set[, 2]) - 1, max(set[, 2] + 1, by=0.01))
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by=0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by=0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
-prob_set = predict(classifier, type='response', newdata=grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+colnames(grid_set) = c('x.LD1', 'x.LD2')
+y_grid = predict(classifier, newdata=grid_set)
 plot(set[, -3],
      main='SVM Classifier (Training Set)',
-     xlab='Age', ylab='Estimated Salary',
+     xlab='LD1', ylab='LD2',
      xlim=range(X1), ylim=range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add=TRUE)
 points(grid_set, pch='.', col=ifelse(
@@ -73,15 +71,14 @@ points(set, pch=21, bg=ifelse(
 # Visualizing the Test set results
 #---------------------------------
 set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1] + 1, by=0.01))
-X2 = seq(min(set[, 2]) - 1, max(set[, 2] + 1, by=0.01))
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by=0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by=0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
-prob_set = predict(classifier, type='response', newdata=grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+colnames(grid_set) = c('x.LD1', 'x.LD2')
+y_grid = predict(classifier, newdata=grid_set)
 plot(set[, -3],
      main='SVM Classifier (Test Set)',
-     xlab='Age', ylab='Estimated Salary',
+     xlab='LD1', ylab='LD2',
      xlim=range(X1), ylim=range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add=TRUE)
 points(grid_set, pch='.', col=ifelse(
